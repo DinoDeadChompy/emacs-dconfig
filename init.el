34726de47e9945689ce4e1b67a8bca26fd576a22
DinoDeadChompy <jsr07kbgo@gmail.com>
2024-06-09 17:50:27 +0100

    added gitignore updated the configs
    added basic configuration to the configs and finally added a gitignore
    to remove the folders emacs creates and from the elpaca package manager

--- init.el
+++ init.el
@@ -1,4 +1,9 @@
+(tool-bar-mode -1)
+(menu-bar-mode -1)
+(scroll-bar-mode -1)
+
 (org-babel-load-file
  (expand-file-name
- "config.org"
- user-emacs-directory))
+  "config.org"
+  user-emacs-directory))
+
